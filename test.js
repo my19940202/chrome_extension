07dab1902eaceee556510efc90a4be9f5485e19e
xishengbo <xishengbo@baidu.com>
2016-03-14 11:52:04 +0800

    在每个广告位iframe之前插入mask div

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var htmlArr = [];
 var htmlStr = '';
-(function(){
+var maskAds = function() {
 	var count = 0
 	var lst = document.getElementsByTagName("iframe")
 	var ids = ""
@@ -9,27 +9,42 @@ var htmlStr = '';
 			var src = lst[i].src
 			if (src.indexOf("pos.baidu.com") > 0 && lst[i].width > 0 && lst[i].height > 0) {
 				ids = ids + "\n" + lst[i].id;
-				lst[i].style.cssText = 'border: 1px solid red;';
+				// lst[i].style.cssText = 'border: 1px solid red;';
 				var tmp = {
 					left: lst[i].offsetLeft,
 					top: lst[i].offsetTop,
 					height: lst[i].offsetHeight,
 					width: lst[i].offsetWidth
 				};
-				var style0 = 'z-index:999;text-align:center;color:#FFF;position:absolute;background:rgba(0,0,0,0.5);font-size:12px;line-height:' + tmp['height'] + 'px;';
+				// 获取tu
+				var tmpsrc = decodeURIComponent(lst[i].src);
+				if (tmpsrc.indexOf('&di') > -1 && tmpsrc.indexOf('&dri') > -1) {
+					var tu = tmpsrc.split('&di=')[1].split('&dri')[0];
+				}
+				var prefixStyle = 'z-index:999;text-align:center;color:#FFF;position:absolute;background:rgba(0,0,0,0.5);font-size:12px;width:100%;line-height:' 
+				+ tmp['height'] + 'px;height:' 
+				+ tmp['height'] +'px;width:' 
+				+ tmp['width'] + 'px;';
 				var style = 'width:' + tmp['width'] + 'px;'
 							+ 'height:' + tmp['height'] + 'px;'
 							+ 'left:' + tmp['left'] + 'px;'
 							+ 'top:' + tmp['top'] + 'px;';
-				htmlStr = '<div style="' + style0 + style + '">' + style + '</div>';
-				htmlArr.push(htmlStr);
+				var innerText = style.replace(/px;/g, ',') + 'tu=' + tu;
+				var adsinfoDiv = document.createElement('div');
+				adsinfoDiv.style.cssText = prefixStyle;
+				adsinfoDiv.innerHTML = innerText;
+				var parentNode = lst[i].parentElement;
+				parentNode.insertBefore(adsinfoDiv,lst[i]);
+
 			}
 		}
 	};
 	var body = document.body;
 	var div = document.createElement('div');
 	div.style.cssText = 'height: 100%;width: 100%;top:0;left:0;';
-	div.innerHTML = htmlArr.join('\n');
+	// div.innerHTML = htmlArr.join('\n');
 	// 感觉这样直接 innerHTML 拼接效率不高
-	body.innerHTML = body.innerHTML + htmlArr.join('\n');
-})();
+	// body.innerHTML = body.innerHTML + htmlArr.join('\n');
+};
+
+setTimeout(maskAds, 1000);
